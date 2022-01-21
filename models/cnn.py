d9982de5f7d59d638eb08dd5e35fb6fd5c10dd2c
Magphik <71084800+Magphik@users.noreply.github.com>
2022-01-21 05:37:46 +0200

    Add model

---
+++ models/cnn.py
@@ -0,0 +1,464 @@
+from tensorflow.keras.layers import Activation, Convolution2D, Dropout, Conv2D
+from tensorflow.keras.layers import AveragePooling2D, BatchNormalization
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import SeparableConv2D
+from tensorflow.keras import layers
+from tensorflow.keras.layers import PReLU
+from tensorflow.keras.regularizers import l2
+import numpy as np
+from tensorflow.python.keras.utils import generic_utils
+from tensorflow.python.util import tf_inspect
+from tensorflow.python.keras.mixed_precision.experimental import policy
+
+from dynamic_func import DyReLU
+from tensorflow.keras import backend as K
+import tensorflow as tf
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras import constraints
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras import regularizers
+# from tensorflow.keras.layers import Layer
+from tensorflow.python.keras.engine.base_layer import Layer
+from tensorflow.tools.docs import doc_controls
+
+class ParametricSoftplus(Layer):
+
+    def __init__(self, alpha, beta, **kwargs):
+        self.initalpha = alpha
+        self.initbeta = beta
+        super(ParametricSoftplus, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.alpha = self.add_weight(name='alpha', shape=(1, 1),
+                                     initializer=initializers.Constant(value=self.initalpha), trainable=True)
+        self.beta = self.add_weight(name='beta', shape=(1, 1), initializer=initializers.Constant(value=self.initbeta),
+                                    trainable=True)
+        super(ParametricSoftplus, self).build(input_shape)  # Be sure to call this somewhere!
+
+    @doc_controls.for_subclass_implementers
+    def call(self, x):
+        # f(x) = alpha * (1 + exp(beta * x))
+        # return self.alpha * K.log(1 + K.exp(self.beta * x))
+        pos = self.beta * K.relu(x)
+        neg = -self.alpha * K.relu(-x)
+        return pos + neg
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'initalpha': self.initalpha,
+            'initbeta': self.initbeta
+            # 'units': self.units,
+            # 'd_model': self.d_model,
+            # 'num_heads': self.num_heads,
+            # 'dropout': self.dropout,
+        })
+        return config
+
+
+def simple_CNN(input_shape, num_classes):
+    model = Sequential()
+    model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same',
+                            name='image_array', input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+    model.add(Dropout(.5))
+
+    model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+    model.add(Dropout(.5))
+
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+    model.add(Dropout(.5))
+
+    model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(AveragePooling2D(pool_size=(2, 2), padding='same'))
+    model.add(Dropout(.5))
+
+    model.add(Convolution2D(filters=256, kernel_size=(3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=num_classes, kernel_size=(3, 3), padding='same'))
+    model.add(GlobalAveragePooling2D())
+    model.add(Activation('softmax', name='predictions'))
+    return model
+
+
+def simpler_CNN(input_shape, num_classes):
+    model = Sequential()
+    model.add(Convolution2D(filters=16, kernel_size=(5, 5), padding='same',
+                            name='image_array', input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=16, kernel_size=(5, 5),
+                            strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(.25))
+
+    model.add(Convolution2D(filters=32, kernel_size=(5, 5), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=32, kernel_size=(5, 5),
+                            strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(.25))
+
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3),
+                            strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(.25))
+
+    model.add(Convolution2D(filters=64, kernel_size=(1, 1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=128, kernel_size=(3, 3),
+                            strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(.25))
+
+    model.add(Convolution2D(filters=256, kernel_size=(1, 1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=128, kernel_size=(3, 3),
+                            strides=(2, 2), padding='same'))
+
+    model.add(Convolution2D(filters=256, kernel_size=(1, 1), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=num_classes, kernel_size=(3, 3),
+                            strides=(2, 2), padding='same'))
+
+    model.add(Flatten())
+    # model.add(GlobalAveragePooling2D())
+    model.add(Activation('softmax', name='predictions'))
+    return model
+
+
+def tiny_XCEPTION(input_shape, num_classes, l2_regularization=0.01):
+    regularization = l2(l2_regularization)
+
+    # base
+    img_input = Input(input_shape)
+    x = Conv2D(5, (3, 3), strides=(1, 1), kernel_regularizer=regularization,
+               use_bias=False)(img_input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(5, (3, 3), strides=(1, 1), kernel_regularizer=regularization,
+               use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # module 1
+    residual = Conv2D(8, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(8, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(8, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 2
+    residual = Conv2D(16, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(16, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(16, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 3
+    residual = Conv2D(32, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(32, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(32, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 4
+    residual = Conv2D(64, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(64, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(64, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    x = Conv2D(num_classes, (3, 3),
+               # kernel_regularizer=regularization,
+               padding='same')(x)
+    x = GlobalAveragePooling2D()(x)
+    output = Activation('softmax', name='predictions')(x)
+
+    model = Model(img_input, output)
+    return model
+
+
+def mini_XCEPTION(input_shape, num_classes, l2_regularization=0.01, weights='encoder', weights_list=None):
+    regularization = l2(l2_regularization)
+
+    # base
+    img_input = Input(input_shape)
+    x = Conv2D(8, (3, 3), strides=(1, 1), kernel_regularizer=regularization,
+               use_bias=False)(img_input)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(8, (3, 3), strides=(1, 1), kernel_regularizer=regularization,
+               use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # module 1
+    residual = Conv2D(16, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(16, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(16, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 2
+    residual = Conv2D(32, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(32, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(32, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 3
+    residual = Conv2D(64, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(64, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(64, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    # module 4
+    residual = Conv2D(128, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(128, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = SeparableConv2D(128, (3, 3), padding='same',
+                        kernel_regularizer=regularization,
+                        use_bias=False)(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x_encdr = layers.add([x, residual])
+
+    if weights == 'encoder' and weights_list is not None:
+        model1 = Model(img_input, x_encdr)
+        # model1.summary()
+        weights_list = weights_list[:-2]
+        model1.set_weights(weights_list)
+        model1.trainable = False
+
+        inputs = Input(input_shape)
+        x = model1(inputs, training=False)
+        x = Dropout(.25)(x)
+        x = GlobalAveragePooling2D()(x)
+        x = layers.Dense(64, activation= ParametricSoftplus(0.1,0.1), name='fc1')(x)
+        x = layers.Dense(32, activation=ParametricSoftplus(0.1,0.1), name='fc2')(x)
+        output = layers.Dense(num_classes, activation='softmax', name='predictions')(x)
+        model = Model(inputs=inputs, outputs=output)
+
+        # x = layers.Dense(64, activation=PReLU(x, name="alpha1"))(x)
+        # x = layers.Dense(32, activation=PReLU)(x)
+        # output = layers.Dense(num_classes, activation='softmax', name='predictions')(x)
+        # model = Model(inputs=inputs, outputs=output)
+
+        weights_list = weights_list[:-3]
+        weights1 = np.random.sample((128,))
+        weights2 = np.random.sample((128, 64))
+        weights3 = np.random.sample((64,))
+        weights4 = np.random.sample((64, 32))
+        weights5 = np.random.sample((32,))
+        weights6 = np.random.sample((32, num_classes))
+        weights7 = np.random.sample((num_classes,))
+        weights_list.append(weights1)
+        weights_list.append(weights2)
+        weights_list.append(weights3)
+        weights_list.append(weights4)
+        weights_list.append(weights5)
+        weights_list.append(weights6)
+        weights_list.append(weights7)
+
+        # print(len(weights_list))
+        # for w in weights_list:
+        #     print(w.shape)
+
+        # print(model.summary())
+    else:
+
+        x = Conv2D(num_classes, (3, 3),
+                   # kernel_regularizer=regularization,
+                   padding='same')(x_encdr)
+        x = GlobalAveragePooling2D()(x)
+        output = Activation('softmax', name='predictions')(x)
+
+        model = Model(img_input, output)
+
+    return model
+
+
+def big_XCEPTION(input_shape, num_classes):
+    img_input = Input(input_shape)
+    x = Conv2D(32, (3, 3), strides=(2, 2), use_bias=False)(img_input)
+    x = BatchNormalization(name='block1_conv1_bn')(x)
+    x = Activation('relu', name='block1_conv1_act')(x)
+    x = Conv2D(64, (3, 3), use_bias=False)(x)
+    x = BatchNormalization(name='block1_conv2_bn')(x)
+    x = Activation('relu', name='block1_conv2_act')(x)
+
+    residual = Conv2D(128, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization(name='block2_sepconv1_bn')(x)
+    x = Activation('relu', name='block2_sepconv2_act')(x)
+    x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization(name='block2_sepconv2_bn')(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+
+    residual = Conv2D(256, (1, 1), strides=(2, 2),
+                      padding='same', use_bias=False)(x)
+    residual = BatchNormalization()(residual)
+
+    x = Activation('relu', name='block3_sepconv1_act')(x)
+    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization(name='block3_sepconv1_bn')(x)
+    x = Activation('relu', name='block3_sepconv2_act')(x)
+    x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False)(x)
+    x = BatchNormalization(name='block3_sepconv2_bn')(x)
+
+    x = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = layers.add([x, residual])
+    x = Conv2D(num_classes, (3, 3),
+               # kernel_regularizer=regularization,
+               padding='same')(x)
+    x = GlobalAveragePooling2D()(x)
+    output = Activation('softmax', name='predictions')(x)
+
+    model = Model(img_input, output)
+    return model
+
+
+if __name__ == "__main__":
+    input_shape = (64, 64, 1)
+    num_classes = 7
+    # model = tiny_XCEPTION(input_shape, num_classes)
+    # model.summary()
+    from tensorflow.keras.models import load_model
+    import numpy as np
+
+    # parameters for loading data and images
+    emotion_model_path = '_mini_XCEPTION.102-0.66.hdf5'
+
+    # hyper-parameters for bounding boxes shape
+    # loading models
+    emotion_classifier = load_model(emotion_model_path, compile=False)
+    list_weights = emotion_classifier.get_weights()
+    model = mini_XCEPTION(input_shape, num_classes, weights_list=list_weights)
+    # model1 = mini_XCEPTION(input_shape, num_classes)
+    model.summary()
+    # model = big_XCEPTION(input_shape, num_classes)
+    # model.summary()
+    # model = simple_CNN((48, 48, 1), num_classes)
+    # model.summary()
